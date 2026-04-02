dd09c34702f84071e9cdbacbbde23907347a21f1
Ayush Verma <108849695+GehraNaksha@users.noreply.github.com>
2026-04-02 14:19:13 +0530

    Update recyclingCentre 4.py

--- recyclingCentre 4.py
+++ recyclingCentre 4.py
@@ -31,7 +31,23 @@
 
 
 #attempt 2
+t = int(input(""))
+# for _ in range(t):
+#     n, c = map(int, input("").split())
+#     a = list(map(int, input("").split()))
 
+#     for _ in range(n):
+#         coins = 0
+#         a.sort(reverse=True)
+#         if a[0] > c and len(a) >= 1:
+#             coins +=1
+#             a.pop(a[0])
+#         else:
+#             a.pop(a[0])
+#     print(coins)
+
+
+##attempt 3
 
 
 testCases = int(input(""))
