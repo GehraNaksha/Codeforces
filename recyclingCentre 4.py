a71a003e7e0d2c69e2d47d07c9784a7e31b6fd89
Ayush Verma <108849695+GehraNaksha@users.noreply.github.com>
2026-04-02 14:19:49 +0530

    Update recyclingCentre 4.py

--- recyclingCentre 4.py
+++ recyclingCentre 4.py
@@ -31,7 +31,7 @@
 
 
 #attempt 2
-t = int(input(""))
+# t = int(input(""))
 # for _ in range(t):
 #     n, c = map(int, input("").split())
 #     a = list(map(int, input("").split()))
@@ -48,7 +48,43 @@ t = int(input(""))
 
 
 ##attempt 3
+# t = int(input(""))
+# for _ in range (t):
+#     n, c = map(int, input("").split())
+#     a = list(map(int, input().split())) 
 
+#     for l in range(n):
+#         coins = 0
+#         difference =[]
+#         b = []
+#         point = float(c/2)        
+#         for j in range(len(a)):
+#             d = a[j] - c/2
+#             b.append(d)
+
+#         b.sort()
+
+#         for o in range(len(a)):
+#             if b[o] > 0:           
+#                 recycled = b[o] + point
+#         for i in range(len(a)):  
+#             print(i)
+#             if a[i] == recycled:
+#                     a.pop(a[i])
+
+#         a = [item*2 for item in a]
+#         print(a)
+#         print(recycled)
+
+#         if recycled > float(c):
+#             coins = coins +1
+#         else:
+#             coins = coins
+# print(coins)
+            
+            
+ ##attempt 4
+#correct soln
 
 testCases = int(input(""))
 
